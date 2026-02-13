76ddeb427d0fa51cb71e957cae5a3100a81de378
Lash00 <mocotign@gmail.com>
2026-02-13 23:52:19 +0200

    Frist 3Sections in the home page Lash

---
+++ src/features/home/home.jsx
@@ -0,0 +1,25 @@
+import Navbar from "./naveBar";
+import themesMAP from "../../../themes/themes";
+import { useState } from "react";
+import Landing from "./landing";
+import ImageSlider from "./imageSlider";
+function HomePage() {
+  const [dark, setDark] = useState(false);
+  return (
+    <div
+      style={{
+        background: dark
+          ? themesMAP["light-main-bg"]
+          : themesMAP["dark-main-bg"],
+        // `linear-gradient(to top right, #ff80b5,${themesMAP["dark-main-bg"]},${themesMAP["dark-main-bg"]})`,
+        minHeight: "100vh",
+      }}
+    >
+      <Navbar dark={dark} setDark={setDark} />
+      <Landing dark={dark} />
+      <ImageSlider dark={dark} />
+    </div>
+  );
+}
+
+export default HomePage;
